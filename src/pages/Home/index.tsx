a2863f7dc2f1ea5d20cc28031500a8e2ae7c7460
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-05-05 18:25:38 +0545

    Home Page Added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,26 @@
-const Home = () => {
-  //   const [spells, setSpells] = useState([]);
+import { buttonVariants } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
-  //   useEffect(() => {
-  //     const fetchedSpell = getAllSpells();
-  //     fetchedSpell.then((data) => {
-  //       setSpells(data.results);
-  //     });
-  //   }, []);
-  //   console.log(spells);
-  return <div>Home</div>;
+const Home = () => {
+  return (
+    <div className='max-w-5xl mx-auto flex flex-col items-center justify-center gap-10'>
+      <img src='/dragon.png' className='' />
+      <img src='/dnd_text.png' className='sm:-mt-20 -mt-10 sm:mr-20 w-40' />
+      <p className='text-lg text-center'>
+        This is the React Assignment to showcase all the spells contained in{' '}
+        <span className='text-gradient font-semibold'>
+          Dungeons & Dragons 5th Edition API
+        </span>
+        . Dungeons & Dragons is a fantasy tabletop role-playing game originally
+        created and designed by Gary Gygax and Dave Arneson. The game was first
+        published in 1974 by Tactical Studies Rules, Inc.
+      </p>
+      <Link to={'/spells'} className={buttonVariants({ variant: 'default' })}>
+        Explore Spells <ArrowRight />
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
